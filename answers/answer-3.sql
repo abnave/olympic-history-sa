0ff70c6f71702e65c0683cede515fc1a5a916846
ShubhamAbnaveInfogenLabs <shubham.a@infogen-labs.com>
2023-05-29 17:39:39 +0530

    added answer-3

--- answers/answer-3.sql
+++ answers/answer-3.sql
@@ -1,1 +1,34 @@
 -- 3. Which player has won maximum gold medals amongst the players which have won only gold medal (never won silver or bronze) over the years.
+SELECT 
+ats.name, 
+Y.total_gold_medals,
+Y.total_other_medals
+FROM
+athlets AS ats
+LEFT JOIN (
+	SELECT X.athlete_id, 
+	SUM(X.gold_medal_count) AS total_gold_medals, 
+	SUM(X.other_medal_count) AS total_other_medals
+	FROM (
+		SELECT 
+		atev.athlete_id,  
+		COUNT(*) AS gold_medal_count, 
+		0 AS other_medal_count
+		FROM athlete_events AS atev
+		WHERE atev.medal = "Gold"
+		GROUP BY atev.athlete_id
+		
+		UNION ALL
+		
+		SELECT 
+		atev.athlete_id, 
+		0 AS gold_medal_count, 
+		COUNT(*) AS other_medal_count
+		FROM athlete_events AS atev
+		WHERE atev.medal = "Silver" OR atev.medal = "Bronze"
+		GROUP BY atev.athlete_id
+	) X
+	GROUP BY X.athlete_id
+	HAVING total_other_medals = 0
+) Y ON Y.athlete_id = ats.id
+ORDER BY Y.total_gold_medals DESC;
